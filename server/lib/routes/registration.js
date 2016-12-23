5cf14d2962995f543b9ddc3d5eff34555660da6a
sucheta mukherjee <sucheta mukherjee>
2016-12-23 13:24:27 -0500

    Issue with registration and server js clean up resolved

--- server/lib/routes/registration.js
+++ server/lib/routes/registration.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
-var registration = require('./lib/registration');
-router.post('/', registration.register);
+var registration = require('../registration');
+//router.post('/', registration.register);
 
 router.get('/', registration.getRegisteredMembers);
 router.get('/:initial', registration.getRegisteredMember);
