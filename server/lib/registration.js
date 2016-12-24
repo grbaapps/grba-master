e1957e9cc96608ac604f7355881318c240ccd5b0
Pritam Sarkar <pritam.sarkar@capitalone.com>
2016-12-23 21:15:50 -0500

    some registration codes

--- server/lib/registration.js
+++ server/lib/registration.js
@@ -3,19 +3,43 @@ var express = require('express');
 var app = express();
 var result = {status:"OK"};
 
+var fs = require("fs");
+
 var registration = {
   //initialize: function(url, apiKey, dbName, authCollection) {
     //passport.use(new MongoStrategy(url, apiKey, dbName, authCollection));
   //},
   register: function(req, res, next) {
-      var data = req.body;
+      var newData = req.body;
       console.log(data);
-    res.json(result);
+      readRegistrationFile(newData);
   },
   getRegisteredMembers: function(req, res, next) {
     res.json(result);
-  }
-  
+  },
+
+
 };
+function readRegistrationFile(newData){
+  var fileName = '../data/+'newData.year+'_registration.json';
+  fs.readFile(fileName, function (err, data) {
+      if (err) throw err;
+      console.log(data);
+      eventsArray = data.events;
+      for(i=0 ; i< events.length; i++){
+        if(events[i]["name"] == newData.eventName){
+          events[i].registrations.push(newData.data);
+        }
+      }
+      //writeToRegistrationFile(eventsArray);
+      console.log(eventsArray);
+
+  }
+
+}
+
+function writeToRegistrationFile(){
+
+}
 
 module.exports = registration;
