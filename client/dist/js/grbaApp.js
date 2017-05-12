8595c4d92f3e540dcec6e64e741b1c53f52b1943
Abhi Ghosh <abhi.ghosh@capitalone.com>
2017-05-12 19:36:38 -0400

    Annual picnic related files for event page

--- client/dist/js/grbaApp.js
+++ client/dist/js/grbaApp.js
@@ -159,7 +159,7 @@ angular.module('event',[]).service('eventService', function($http, $log, $q) {
     
      this.getRegistrationDetails =  function() {
          var deferred = $q.defer();
-        $http.get('/api/registration/year/2017/event/PB')
+        $http.get('/api/registration/year/2017/event/AP')
             .success(function(data) {
 
                 //this.currentEvent = data;
