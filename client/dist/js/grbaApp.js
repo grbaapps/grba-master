165fbf61990ff1e996e561eb1c8f89f342106bfc
Surajit Pal <spal@palmac.home>
2017-08-08 00:59:58 -0400

    added pictures of board members

--- client/dist/js/grbaApp.js
+++ client/dist/js/grbaApp.js
@@ -1,4 +1,4 @@
-/*! grbaApp - v0.0.1-SNAPSHOT - 2017-08-05
+/*! grbaApp - v0.0.1-SNAPSHOT - 2017-08-08
  * https://github.com/angular-app/angular-app
  * Copyright (c) 2017 Surajit Pal/Abhishek Ghosh;
  * Licensed MIT
