89aa5eda7a9c902180f60e4bd6782894dacb8791
Abhi Ghosh <abhi.ghosh@capitalone.com>
2017-08-22 19:46:35 -0400

    Early bird date changed to 10th September

--- client/dist/js/grbaApp.js
+++ client/dist/js/grbaApp.js
@@ -1,4 +1,4 @@
-/*! grbaApp - v0.0.1-SNAPSHOT - 2017-08-21
+/*! grbaApp - v0.0.1-SNAPSHOT - 2017-08-22
  * https://github.com/angular-app/angular-app
  * Copyright (c) 2017 Surajit Pal/Abhishek Ghosh;
  * Licensed MIT
