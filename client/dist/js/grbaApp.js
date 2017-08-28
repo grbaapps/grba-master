0dc0e268515dacd14a27b6d1fbce824a94d02573
Surajit Pal <spal@palmac.home>
2017-08-28 01:18:47 -0400

    Updated color for footer icons

--- client/dist/js/grbaApp.js
+++ client/dist/js/grbaApp.js
@@ -1,4 +1,4 @@
-/*! grbaApp - v0.0.1-SNAPSHOT - 2017-08-27
+/*! grbaApp - v0.0.1-SNAPSHOT - 2017-08-28
  * https://github.com/angular-app/angular-app
  * Copyright (c) 2017 Surajit Pal/Abhishek Ghosh;
  * Licensed MIT
