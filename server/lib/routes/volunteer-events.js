09311cfcc0e15f96b4749150462a674cc820324d
pritamsarkar007 <pritam.rock86@gmail.com>
2018-02-19 11:35:07 -0500

    bug fix

--- server/lib/routes/volunteer-events.js
+++ server/lib/routes/volunteer-events.js
@@ -4,7 +4,7 @@ var volunteerApi = require('../volunteer-event-api');
 
 //validator middleware
 
-router.get('/year/:year/volunteer-event/:eventCode',volunteerApi.get);
+router.get('/year/:year/events/:eventCode',volunteerApi.get);
 router.get('/year/:year',volunteerApi.list);
 
 module.exports = router;
--- server/lib/volunteer-event-api.js
+++ server/lib/volunteer-event-api.js
@@ -3,27 +3,35 @@ var aws = require('aws-sdk');
 var volunteerEvent = {
     get: function(req, res, next) {
         try {
-            var fileName = req.params.year + '_volunteer_events.json';
-            var config = req.globalConfig;
-            var s3 = new aws.S3({
-                "accessKeyId": process.env.accessKey,
-                "secretAccessKey": process.env.secretAccessKey
-            });
+            if (req.params.eventCode) {
+                var fileName = req.params.year + '_volunteer_events.json';
+                var config = req.globalConfig;
+                var s3 = new aws.S3({
+                    "accessKeyId": process.env.accessKey,
+                    "secretAccessKey": process.env.secretAccessKey
+                });
 
-            var params = {
-                "Bucket": config.aws.s3Bucket,
-                "Key": fileName,
+                var params = {
+                    "Bucket": config.aws.s3Bucket,
+                    "Key": fileName,
 
-            }
-            logger.info("*****The accessKeyId is : "+process.env.accessKey);
-            logger.info("*****The secretAccessKey is : "+process.env.secretAccessKey);
-            logger.debug("params : " + JSON.stringify(params))
-            var fileData = "";
-            s3.getObject(params, function(err, data) {
-                if (err) {
-                    logger.error(err);
-                    if (err.statusCode && err.statusCode == 404) {
-                        //Registration file does not exist. Throw 404
+                }
+                var fileData = "";
+                s3.getObject(params, function(err, data) {
+                    if (err) {
+                        logger.error(err);
+                        if (err.statusCode && err.statusCode == 404) {
+                            //Registration file does not exist. Throw 404
+                            var returnObj = {
+                                "status": 404,
+                                "message": "There are no volunteer events available."
+                            }
+                            res.status(404);
+                            return res.send(returnObj);
+                        } else {
+                            return next(err);
+                        }
+                    } else if (data.Body.length == 0) {
                         var returnObj = {
                             "status": 404,
                             "message": "There are no volunteer events available."
@@ -31,34 +39,27 @@ var volunteerEvent = {
                         res.status(404);
                         return res.send(returnObj);
                     } else {
-                        return next(err);
-                    }
-                } else if (data.Body.length == 0) {
-                    var returnObj = {
-                        "status": 404,
-                        "message": "There are no volunteer events available."
-                    }
-                    res.status(404);
-                    return res.send(returnObj);
-                } else {
-                    data = JSON.parse(data.Body.toString('utf-8'));
-                    logger.warn(JSON.stringify(data));
-                    mainEvent = data.events[req.params.eventCode];
-                    if (mainEvent) {
-                        res.status(200);
-                        return res.send(mainEvent.details);
+                        data = JSON.parse(data.Body.toString('utf-8'));
 
-                    } else {
-                        //event does not exist. throw 404
-                        var returnObj = {
-                            "status": 404,
-                            "message": "There are no volunteer event available for this eventcode."
+                        mainEvent = data.events.filter(event => event.eventCode == req.params.eventCode);
+                        if (mainEvent.length > 0 ) {
+                            res.status(200);
+                            return res.send(mainEvent);
+
+                        } else {
+                            //event does not exist. throw 404
+                            var returnObj = {
+                                "status": 404,
+                                "message": "There are no volunteer event available for this eventcode."
+                            }
+                            res.status(404);
+                            return res.send(returnObj);
                         }
-                        res.status(404);
-                        return res.send(returnObj);
                     }
-                }
-            });
+                });
+            } else {
+                return this.list(req, res, next);
+            };
         } catch (e) {
             res.status(500);
             return next(e);
@@ -102,6 +103,7 @@ var volunteerEvent = {
                     res.status(404);
                     return res.send(returnObj);
                 } else {
+
                     data = JSON.parse(data.Body.toString('utf-8'));
                     res.status(200);
                     return res.send(data.events);
