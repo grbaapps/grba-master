9455aa669b5dfe8c4bea897aae397bb7fa4b361e
Abhishek <abhi.unc@gmail.com>
2018-09-02 15:12:03 -0400

    Changes for Durga Puja 2018

--- client/src/app/registration/registration.js
+++ client/src/app/registration/registration.js
@@ -1,7 +1,7 @@
 angular.module('registration', ['event']).controller('registrationController', ['$scope', '$http', '$resource', '$log', 'eventService', function ($scope, $http, $resource, $log, eventService) {
     $scope.showRegForm = true;
     $scope.showRegResult = false;
-    
+
 
     $scope.submit = function () {
         $http({
@@ -19,7 +19,9 @@ angular.module('registration', ['event']).controller('registrationController', [
                     isStudent: $scope.isStudent,
                     isVegetarian: $scope.isVegetarian,
                     noOfAdults: Number($scope.noOfAdults),
-                    noOfChildren: Number($scope.noOfChildren),
+                    noOfChildren0To3: Number($scope.noOfChildren0To3),
+                    noOfChildren4To12: Number($scope.noOfChildren4To12),
+                    noOfChildren12Above: Number($scope.noOfChildren12Above),
                     eventFee: $scope.eventFee,
                     sponsorshipCategory: $scope.sponsorshipCategory,
                     specialNote: $scope.specialNote
