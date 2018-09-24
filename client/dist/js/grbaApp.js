b4b2d330ea9a1c49af56d388677f2419a065210e
Abhishek <abhi.unc@gmail.com>
2018-09-24 12:08:27 -0400

    Added ad for stemtree

--- client/dist/js/grbaApp.js
+++ client/dist/js/grbaApp.js
@@ -1,4 +1,4 @@
-/*! grbaApp - v0.0.1-SNAPSHOT - 2018-09-03
+/*! grbaApp - v0.0.1-SNAPSHOT - 2018-09-24
  * https://github.com/angular-app/angular-app
  * Copyright (c) 2018 Surajit Pal/Abhishek Ghosh;
  * Licensed MIT
